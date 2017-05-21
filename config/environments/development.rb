766f7d132ba6908c81e7c13203017befd526efe7
Oleg Cherednichenko <olegh.cherednichenko@gmail.com>
2017-05-21 15:49:07 +0300

    react setup and bootstrap

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  # Add React.js to pipeline
+  config.react.variant = :development
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
